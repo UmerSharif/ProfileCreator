1df17bfea5cd74b7ec14300273acd75c50aba198
umer <umer4167@hotmail.com>
2019-10-31 18:50:17 +0200

    removing redux depencies and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import store from "./store/store";
-import Test from "./components/Test";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Profile Creator</h1>
       <p>Hello world</p>
-      <Test store={store} />
+      
     </div>
   );
 }
