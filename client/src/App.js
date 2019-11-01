4a443304e94f1e8da603418485351c36830323f6
umer <umer4167@hotmail.com>
2019-11-01 19:05:27 +0200

    connecting back to front

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import store from "./store/store";
-
 
 function App() {
   return (
     <div className="App">
       <h1>Profile Creator</h1>
       <p>Hello world</p>
-      
     </div>
   );
 }
